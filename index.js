52614bf905960dee5f8211cb9300784eb47e35fa
PrincessLunaZ <74730757+PrincessLunaZ@users.noreply.github.com>
2021-06-13 09:38:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
+// ODQ0NDQyOTk4ODcxNjg3MjA4.YKSe0w.1ihppILyDv8g47TX0fG8Zp7Tb1o
 const EconomyClient = require("./structures/Client");
 new EconomyClient().start(require("./config").token, "./commands");
